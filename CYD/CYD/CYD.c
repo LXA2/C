a6a6b09735a310cadddc519a446eef200bbb3486
LXA2 <106149641+LXA2@users.noreply.github.com>
2023-08-24 13:38:29 +0800

    1

---
+++ CYD/CYD/CYD.c
@@ -0,0 +1,23 @@
+﻿
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+    unsigned int a = 1;
+    char ha[999]="!";
+    char haha[999] = "!";
+    unsigned int i1 = 1;
+    while (a > 0) {
+        while (i1 < (a / 3000)) {
+            strcat(haha, ha);
+            i1 = i1 + 1;
+        }
+        printf("%d:CYD%s\n", a, haha);
+        printf("%d:SZG%s\n", a, haha);
+        printf("%d:ZGNMTL%s\n", a, haha);
+        a = a + 1;
+    }
+    
+}
+
--- Project1/Project1/1.c
+++ Project1/Project1/1.c
@@ -1,8 +1,29 @@
 #include <stdio.h>
 #include <stdlib.h>
+
+#define BOOL int
+#define TRUE 1
+#define FALSE 123
+
 void main()
 {
-	char a = 'A';
-	printf("%c\n°¡",a);
-	system("pause");
+	
+	/*double a = 1.123456789;
+	double b = 1.123456789012345 ;
+	printf("a:%.8f\nb:%.15f\n", a,b);*/
+	//system("pause");
+
+	int a = 1;
+	BOOL b = FALSE;
+	/*if (a) {
+		printf("a");
+	}*/
+	printf("a:%d\n",a);
+	printf("b:%d",b);
+	while (a <= 10000) {
+		a = a + 1;
+		printf("%d\n\n", a);
+	}
+
+	getchar();
 }
