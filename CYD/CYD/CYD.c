74e6a5c2e8f6d3c1b960912a403a8a3840a73db9
LXA2 <106149641+LXA2@users.noreply.github.com>
2023-08-25 10:04:51 +0800

    1

--- CYD/CYD/CYD.c
+++ CYD/CYD/CYD.c
@@ -4,19 +4,29 @@
 
 int main()
 {
-    unsigned int a = 1;
-    char ha[999]="!";
-    char haha[999] = "!";
-    unsigned int i1 = 1;
+    unsigned int a = 1;//number of lines
+    char ha[2]="!";
+    char haha[10000] = "!";
+    char hahaha[99999] = "";
+    char khlnb[10] = "KHLNB!!!";
+    char blank[2]="";
+    unsigned int i1 = 1;//number of exclamation points
     while (a > 0) {
-        while (i1 < (a / 3000)) {
+        
+        strcpy(hahaha, blank);
+        while (i1 < (a / 1000)) {
             strcat(haha, ha);
-            i1 = i1 + 1;
+            i1++;
         }
-        printf("%d:CYD%s\n", a, haha);
-        printf("%d:SZG%s\n", a, haha);
-        printf("%d:ZGNMTL%s\n", a, haha);
-        a = a + 1;
+        int i2 = 1;
+        while (i2 < (a % 100000)/5000) {
+            strcat(hahaha, khlnb);
+            i2++;
+        }
+        printf("%sKHLNB%s\n",hahaha, haha);
+        //printf("%d:SZG%s\n", a, haha);
+        //printf("%d:ZGNMTL%s\n", a, haha);
+        a++;
     }
     
 }
